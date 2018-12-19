16cf09d90220937a7e61bab2e23d9aacb22f3e7c
ram-smart <ramsmart@mail.ru>
2018-12-19 20:35:26 +0500

    created files Main.java Point.java and Player.java

---
+++ src/gameXO/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String... args) {
+		final Point p = new Point();
+		p.x = 2;
+		p.y = 3;
+
+		final Player player = new Player();
+		player.name = "Slava";
+		player.figure = "X";
+	}
+
+}
